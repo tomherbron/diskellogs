f5591f56df3f941034ac97efa88ba24302119f0a
unknown <tom.herbron@proton.me>
2023-10-29 12:01:25 +0100

    Added registration reactive form with api auth service to register user

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import {HttpService} from "./http.service";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private registerEndpoint = 'register'
+
+  constructor(private httpService: HttpService) { }
+
+  registerUser(userData: any): Observable<any> {
+    return this.httpService.post(this.registerEndpoint, userData);
+  }
+
 }
--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  private baseUrl = 'http://127.0.0.1:5000';
+
+  get(endpoint: string): Observable<any> {
+    return this.httpClient.get(`${this.baseUrl}/${endpoint}`);
+  }
+
+  post(endpoint: string, data: any): Observable<any> {
+    return this.httpClient.post(`${this.baseUrl}/${endpoint}`, data);
+  }
+
+  put(endpoint: string, data: any): Observable<any> {
+    return this.httpClient.put(`${this.baseUrl}/${endpoint}`, data);
+  }
+
+  delete(endpoint: string): Observable<any> {
+    return this.httpClient.delete(`${this.baseUrl}/${endpoint}`);
+  }
+
 }
